1df9bfd2a46c676aaf9a4c4d09873591deda79bb
Fergus Leen <leenf@tcd.ie>
2015-12-02 14:38:55 +0000

    init

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+(function() {
+
+function MainController($scope) {
+  var self = this;
+
+}
+
+angular.module('awsServersApp')
+  .controller('MainController', MainController);
+  
+})();
